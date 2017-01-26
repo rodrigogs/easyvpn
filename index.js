5f09fda015afd8fb39b91432093f6b4b21e000da
rodrigo.smscom@gmail.com <rgs84060989>
2017-01-26 19:31:39 -0200

    fixed query country problem

--- index.js
+++ index.js
@@ -78,6 +78,8 @@ function execute(country, query) {
               resolve(vpns);
             })
             .catch(reject);
+        } else {
+          resolve(vpns);
         }
       });
     })
